09d6874966fc2ba90013b8302e5f35f183840c0e
Ruslan-Karnaukh <115695997+Ruslan-Karnaukh@users.noreply.github.com>
2023-01-16 14:57:07 +0000

    Deploying to gh-pages from @ Ruslan-Karnaukh/plombiro@7a78e2df7a390dd3dfb129292d22718e19da2780 🚀

---
+++ index.0fdaeb96.js
@@ -0,0 +1,2 @@
+var scrollBtn=document.querySelector(".btn-scroll"),btnVisibility=function(){window.scrollY>400?scrollBtn.style.visibility="visible":scrollBtn.style.visibility="hidden"};document.addEventListener("scroll",(function(){btnVisibility()})),scrollBtn.addEventListener("click",(function(){window.scrollTo({top:0,behavior:"smooth"})}));
+//# sourceMappingURL=index.0fdaeb96.js.map
